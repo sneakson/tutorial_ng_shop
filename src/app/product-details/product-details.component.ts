b6edfc9b1ac40e1ab87ea321a02113d0d8113ee1
Mark Anthony Sison <msison06@gmail.com>
2020-01-23 18:55:32 -0600

    feat(cart): stub adding items to cart

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { CartService } from '../cart.service';
 import { products } from '../products';
 
 @Component({
@@ -12,11 +13,19 @@ export class ProductDetailsComponent implements OnInit {
 
     product;
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(
+        private route: ActivatedRoute,
+        private cartService: CartService
+    ) { }
 
     ngOnInit() {
         this.route.paramMap.subscribe(params => {
             this.product = products[+params.get('productId')];
         })
+    }
+
+    addToCart(product){
+        window.alert("You have added the " + product.name + " to the cart.");
+        this.cartService.addToCart(product);
     }
 }
